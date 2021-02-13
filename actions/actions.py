4871c25856c0098687a97e47aeefd82e57d459c6
mistadrumma <breakroger104@gmail.com>
2021-02-13 22:05:26 +0300

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,31 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+
+class ActionDirectionSearch(Action):
+
+    def name(self) -> Text:
+        return "action_direction_search"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        direction = tracker.get_slot("direction")
+        location = tracker.get_slot("location")
+        adress = "Улица Поляны 9"
+
+        dispatcher.utter_message(text="Это адрес {} в {}:{}".format(direction, location, adress))
+
+        return [SlotSet("adress", adress)]
